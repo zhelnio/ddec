bf426c3d90fe0e40eab685f1572ced51931ae012
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-09-12 15:03:18 +0300

    dir structure refactoring

--- lab/11_comb_simple/run/script_modelsim.tcl
+++ lab/11_comb_simple/run/script_modelsim.tcl
@@ -3,8 +3,8 @@
 vlib work
 
 # compile all the Verilog sources
-vlog     ../*.v 
-vlog -sv ../*.sv 
+vlog     ../rtl/*.v 
+vlog -sv ../tb/*.sv 
 
 # open the testbench module for simulation
 vsim -novopt work.testbench
---
+++ lab/11_comb_simple/run/script_quartus.tcl
@@ -0,0 +1,4 @@
+
+set_global_assignment -name TOP_LEVEL_ENTITY comb_simple
+
+set_global_assignment -name VERILOG_FILE ../rtl/comb_simple.v
