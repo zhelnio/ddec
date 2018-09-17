c8977d73c8b3d77805753ccf6e9be29ba336e0d2
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-09-17 14:49:00 +0300

    bugfix

--- lab/12_comb_mux_1bit/rtl/lab_top.v
+++ lab/12_comb_mux_1bit/rtl/lab_top.v
@@ -15,6 +15,6 @@ module lab_top
     b1_mux_2_1_sel   b1_mux_2_1_sel   (   idata0, idata1,   sel, y_sel   );
     b1_mux_2_1_if    b1_mux_2_1_if    (   idata0, idata1,   sel, y_if    );
     b1_mux_2_1_case  b1_mux_2_1_case  (   idata0, idata1,   sel, y_case  );
-    b1_mux_2_1_index b1_mux_2_1_index ( { idata0, idata1 }, sel, y_index );
+    b1_mux_2_1_index b1_mux_2_1_index ( { idata1, idata0 }, sel, y_index );
 
 endmodule
