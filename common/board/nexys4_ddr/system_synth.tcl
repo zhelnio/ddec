9d0de31a32cbc39e1196862f9b36012b7b6879c7
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-09-20 17:59:57 +0300

    vivado scripts update

--- common/board/nexys4_ddr/system_synth.tcl
+++ common/board/nexys4_ddr/system_synth.tcl
@@ -11,9 +11,9 @@ launch_runs $synth_task -jobs 2
 wait_on_run -verbose $synth_task
 
 # run implementation
-launch_runs $impl_task  -jobs 2 -to_step write_bitstream
+launch_runs $impl_task  -jobs 2 
 wait_on_run -verbose $impl_task
 
-# create the timing report
+# write bitstream
 open_run $impl_task -name $impl_task
-report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets
+write_bitstream "$project_name.bit"
