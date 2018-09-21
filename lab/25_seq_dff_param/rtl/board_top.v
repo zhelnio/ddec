eb2ea37d99d7930533d43e243646dcfe0c776723
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-09-21 15:40:14 +0300

    lab 25 refactoring

--- lab/25_seq_dff_param/rtl/board_top.v
+++ lab/25_seq_dff_param/rtl/board_top.v
@@ -4,19 +4,29 @@ module board_top
     input  [ 9:0] SW,
     output [ 9:0] LED
 );
-    assign LED[0] = 1'b0;
+    wire clk   = KEY[1];
+    wire rst_n = SW [0];
+
+    localparam WIDTH = 6;
+    localparam ZEROD = { WIDTH {1'b0} };
+
+    wire [WIDTH-1:0] d = SW [1 +: WIDTH];
+    wire [WIDTH-1:0] q;
 
     dff_async_rst_n_param 
     #(
-        .WIDTH ( 8     ), 
-        .RESET ( 8'hf0 )
+        .WIDTH ( WIDTH ), 
+        .RESET ( ZEROD )
     ) 
     dff_async_rst_n_param
     (
-        .clk   ( ~KEY  [0]   ),
-        .rst_n (  SW   [0]   ),
-        .d     (  SW   [9:1] ),
-        .q     (  LED  [9:1] )
+        .clk   ( clk   ),
+        .rst_n ( rst_n ),
+        .d     ( d     ),
+        .q     ( q     )
     );
+
+    assign LED[      0] = rst_n;
+    assign LED[WIDTH:1] = q;
 
 endmodule
